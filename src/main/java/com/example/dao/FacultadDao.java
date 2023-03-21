76bf246695437f3e4326e08e6cfaecffebbbf57f
vrmachado <vrmachado@gmail.com>
2023-03-21 10:35:12 +0100

    Repository Annotation removed

--- src/main/java/com/example/dao/FacultadDao.java
+++ src/main/java/com/example/dao/FacultadDao.java
@@ -1,11 +1,9 @@
 package com.example.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.entities.Facultad;
 
-@Repository
 public interface FacultadDao extends JpaRepository<Facultad, Integer> {
     
 }
--- src/main/java/com/example/dao/TelefonoDao.java
+++ src/main/java/com/example/dao/TelefonoDao.java
@@ -1,11 +1,9 @@
 package com.example.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.entities.Telefono;
 
-@Repository
 public interface TelefonoDao extends JpaRepository<Telefono, Integer> {
     
 }
