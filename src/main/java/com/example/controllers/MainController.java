0aa95450dafc729550f91d56dbfc6b38839aeb96
vrmachado <vrmachado@gmail.com>
2023-03-21 14:30:25 +0100

    Sincronizada con la rama formulario

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -50,5 +51,13 @@ public class MainController {
         model.addAttribute("facultades", facultades);
 
         return "views/formularioAltaEstudiante";
+    }
+
+    /**
+     * Metodo que recibe los datos procedente de los controles del formulario
+     */
+    @PostMapping("/altaEstudiante")
+    public void altaEstudiante() {
+
     }
 }
